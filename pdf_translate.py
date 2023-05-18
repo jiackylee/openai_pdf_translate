8aaba80c3760e18ebd6ecbe69a6fe0ce05946696
XIA <xiaguangzhong@gmail.com>
2023-05-18 16:28:07 +0800

    优化翻译方式

--- pdf_translate.py
+++ pdf_translate.py
@@ -1,68 +1,93 @@
+import os
+import json
 import textwrap
 import openai
 import pdfplumber
-from reportlab.lib.pagesizes import letter, landscape
-from reportlab.lib import colors
+from reportlab.lib.pagesizes import letter
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
+import nltk
 
-# Register the font, make sure the font file is available in the current directory
 pdfmetrics.registerFont(TTFont("SimFang", "simfang.ttf"))
 
 def get_api_keys():
     api_keys = []
-    print("Enter your OpenAI API keys, one per line. Type 'END' to finish:")
+    print("Enter your OpenAI API keys, one per line. Type 'EOF' to finish:")
     while True:
         api_key = input()
-        if api_key.strip().upper() == "END":
+        if api_key.strip().upper() == "EOF":
             break
-        api_keys.append(api_key.strip())  # Strip spaces from the input
+        api_keys.append(api_key)
     return api_keys
 
-def get_lines_from_pdf(filename):
+def get_paragraphs_from_pdf(filepath):
     try:
-        with pdfplumber.open(filename) as pdf:
-            lines = []
+        with pdfplumber.open(filepath) as pdf:
+            paragraphs = []
             for page in pdf.pages:
                 text = page.extract_text()
-                if text:  # Check if text is not None
-                    for line in text.split('\n'):
-                        lines.append(line)
-            return lines
+                if text:
+                    for paragraph in text.split('\n\n'):
+                        # Use NLTK's Punkt sentence tokenizer
+                        sentences = nltk.tokenize.sent_tokenize(paragraph)
+                        for sentence in sentences:
+                            wrapped_lines = textwrap.wrap(sentence, width=50)
+                            paragraphs.extend(wrapped_lines)
+            return paragraphs
     except FileNotFoundError:
-        print(f"File not found: {filename}")
+        print(f"File not found: {filepath}")
         return None
 
-def translate_text(text_to_translate, target_language, api_keys):
+def translate_text(text_to_translate, target_language, api_keys, start_index=0, start_segment=0):
     translations = []
     current_key_index = 0
     openai.api_key = api_keys[current_key_index]
 
-    for index, line in enumerate(text_to_translate):
-        segments = textwrap.wrap(line, width=200)
-        for segment in segments:
-            if not openai.api_key:
-                raise Exception("All API keys have been exhausted.")
+    try:
+        with open('translation_state.json', 'r') as f:
+            state = json.load(f)
+            start_index = state['index']
+            start_segment = state['segment']
+            translations = state['translations']
+    except FileNotFoundError:
+        pass
+
+    for index in range(start_index, len(text_to_translate)):
+        paragraph = text_to_translate[index]
+        segments = textwrap.wrap(paragraph, width=200)
+        for segment_index in range(start_segment, len(segments)):
+            segment = segments[segment_index]
             try:
                 response = openai.Completion.create(
                     engine="text-davinci-003",
                     prompt=f"Translate the following English text to {target_language}:\n\n{segment}\n",
-                    max_tokens=1024,
+                    max_tokens=200,
                     n=1,
                     stop=None,
                     temperature=0.1,
                 )
                 translation = response.choices[0].text.strip()
                 translations.append(translation)
-                print(f"Translated line {index + 1}/{len(text_to_translate)}")
+                print(f"Translated paragraph {index + 1}/{len(text_to_translate)}")
+
+                with open('translation_state.json', 'w') as f:
+                    json.dump({
+                        'index': index,
+                        'segment': segment_index + 1,
+                        'translations': translations,
+                    }, f)
+
             except openai.error.RateLimitError:
                 if current_key_index + 1 < len(api_keys):
                     current_key_index += 1
                     openai.api_key = api_keys[current_key_index]
                 else:
-                    openai.api_key = None
+                    raise Exception("All API keys have been exhausted.")
+
+    os.remove('translation_state.json')
+
     return translations
 
 def save_translation_to_pdf(original_text, translated_text, filename):
@@ -81,25 +106,19 @@ def save_translation_to_pdf(original_text, translated_text, filename):
 
     doc.build(elements)
 
-# 获取用户的API密钥列表
 api_keys = get_api_keys()
 
-# 获取要翻译的文件名
-input_filename = input("Enter the name of the PDF file you want to translate: ")
+input_filepath = input("Enter the path of the PDF file you want to translate: ")
 
-# 从PDF文件中获取要翻译的多个行
-text_to_translate = get_lines_from_pdf(input_filename)
+text_to_translate = get_paragraphs_from_pdf(input_filepath)
 
+target_language = input("Enter the target language (e.g., Chinese, Spanish, French): ")
 
-if text_to_translate is None:
-    print("Cannot proceed without valid PDF file.")
-else:
-    target_language = input("Enter the target language (e.g., Chinese, Spanish, French): ")  # 获取目标语言
+translated_text = translate_text(text_to_translate, target_language, api_keys)
 
-    translated_text = translate_text(text_to_translate, target_language, api_keys)  #翻译文本
-    input_filename_base = input_filename.rsplit('.', 1)[0] if '.' in input_filename else input_filename #获取文件的原名称
-    output_filename = f"{input_filename_base}_translated.pdf"  #用原文件名字生成输出文件名
-
-# 将原始文本和翻译结果保存到一个PDF文件    
+base_name = os.path.basename(input_filepath)
+base_name_without_ext = os.path.splitext(base_name)[0]
+output_filename = base_name_without_ext + "_translated.pdf"
 save_translation_to_pdf(text_to_translate, translated_text, output_filename)
+
 print(f"Translation and original text have been saved to {output_filename}")
